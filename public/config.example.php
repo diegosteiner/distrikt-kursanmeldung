3fc34849579225a1c418520afc96381c2018326c
Diego Steiner <939106+diegosteiner@users.noreply.github.com>
2024-10-17 09:14:07 +0200

    Update config.example.php

--- public/config.example.php
+++ public/config.example.php
@@ -2,6 +2,17 @@
 
 $config = [
   'base_url' => '',
+  'kurse' => [
+    'pio_ostern' => 'Piokurs Ostern', 
+    'pio_fruehling' => 'Piokurs Frühlingsferien', 
+    'futura_ostern' => 'Futurakurs Ostern', 
+    'futura_unterwegs' => 'Futurakurs Ostern (unterwegs)',                                              
+    'futura_herbst' => 'Futurakurs Herbstferien', 
+    'basis_wolf' => 'Basiskurs Wolfsstufe', 
+    'basis_pfadi' => 'Basiskurs Pfadistufe',                                               
+    'aufbau_wolf' => 'Aufbaukurs Wolfsstufe', 
+    'aufbau_pfadi' => 'Aufbaukurs Pfadistufe'
+  ],
   'abteilungen' => [
     'morea' => '',
     'agua' => '',
